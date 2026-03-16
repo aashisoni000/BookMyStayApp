1f97ad1e9d5355ff043ac081f077eb7d760d6090
aashisoni000 <aashi2701soni@gmail.com>
2026-03-16 15:02:55 +0530

    Inventory System using hash map

--- BookMyStayApp.java
+++ BookMyStayApp.java
@@ -1,4 +1,7 @@
-// UC2
+import java.util.HashMap;
+import java.util.Map;
+
+// --- UC2: Domain Model ---
 abstract class Room {
     private String type;
     private int beds;
@@ -10,57 +13,82 @@ abstract class Room {
         this.price = price;
     }
 
+    public String getType() { return type; }
 
     public void displayInfo() {
         System.out.println("Room Type: " + type + " | Beds: " + beds + " | Price: $" + price);
     }
 }
 
-class SingleRoom extends Room {
-    public SingleRoom() {
-        super("Single Room", 1, 100.0);
-    }
-}
+class SingleRoom extends Room { public SingleRoom() { super("Single", 1, 100.0); } }
+class DoubleRoom extends Room { public DoubleRoom() { super("Double", 2, 180.0); } }
+class SuiteRoom extends Room { public SuiteRoom() { super("Suite", 3, 350.0); } }
 
-class DoubleRoom extends Room {
-    public DoubleRoom() {
-        super("Double Room", 2, 180.0);
-    }
-}
+// --- UC3: Centralized Room Inventory Management ---
+class RoomInventory {
+    private Map<String, Integer> inventory;
 
-class SuiteRoom extends Room {
-    public SuiteRoom() {
-        super("Suite Room", 3, 350.0);
+    public RoomInventory() {
+        this.inventory = new HashMap<>();
+    }
+
+    public void addRoomType(String type, int count) {
+        inventory.put(type, count);
+    }
+
+    // Controlled update: logic for booking or cancelling can be added here later
+    public void updateAvailability(String type, int count) {
+        if (inventory.containsKey(type)) {
+            inventory.put(type, count);
+        }
+    }
+
+    public int getAvailability(String type) {
+        return inventory.getOrDefault(type, 0);
+    }
+
+    public void displayInventory() {
+        System.out.println("--- Current Room Inventory ---");
+        for (Map.Entry<String, Integer> entry : inventory.entrySet()) {
+            System.out.println(entry.getKey() + " Rooms Available: " + entry.getValue());
+        }
+        System.out.println("------------------------------");
     }
 }
 
 public class BookMyStayApp {
-
-    static int singleRoomAvailability = 5;
-    static int doubleRoomAvailability = 3;
-    static int suiteRoomAvailability = 1;
-
-    //UC1
     public static void main(String[] args) {
-        System.out.println("Welcome to Book My Stay v1.1");
+        // UC1: Welcome Message
+        System.out.println("Welcome to Book My Stay v1.2");
         System.out.println("----------------------------");
 
+        // UC3: Initialize Centralized Inventory
+        RoomInventory hotelInventory = new RoomInventory();
+
+        hotelInventory.addRoomType("Single", 5);
+        hotelInventory.addRoomType("Double", 3);
+        hotelInventory.addRoomType("Suite", 1);
+
+        // UC2: Polymorphism with Room Objects
         Room single = new SingleRoom();
         Room dual = new DoubleRoom();
         Room suite = new SuiteRoom();
-
         single.displayInfo();
-        System.out.println("Available: " + singleRoomAvailability);
+        System.out.println("Live Inventory: " + hotelInventory.getAvailability(single.getType()));
         System.out.println();
 
         dual.displayInfo();
-        System.out.println("Available: " + doubleRoomAvailability);
+        System.out.println("Live Inventory: " + hotelInventory.getAvailability(dual.getType()));
         System.out.println();
 
         suite.displayInfo();
-        System.out.println("Available: " + suiteRoomAvailability);
+        System.out.println("Live Inventory: " + hotelInventory.getAvailability(suite.getType()));
         System.out.println();
 
-        System.out.println("End of Room Inventory.");
+        System.out.println("Update: One Suite Room Booked...");
+        hotelInventory.updateAvailability("Suite", 0);
+
+        hotelInventory.displayInventory();
+        System.out.println("End of Application Execution.");
     }
 }
