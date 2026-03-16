1967e4301c581d24e7d40d094bbbf1e4c692706c
unknown <zad97136@gmail.com>
2026-03-16 14:18:14 +0530

    UC1

---
+++ BookMyStayApp.java
@@ -0,0 +1,8 @@
+public class BookMyStayApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Book My Stay!");
+        System.out.println("System: Hotel Booking Management System");
+        System.out.println("Version: 1.0");
+
+    }
+}
